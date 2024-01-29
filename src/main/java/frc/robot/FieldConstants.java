3f62b1f5557e22e5da53dd7331428a81a82e03e9
bauclair22 <59893555+bauclair22@users.noreply.github.com>
2024-01-29 09:55:35 -0500

    Create FieldConstants.java

---
+++ src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+//Add any constants relating to the field here
+public class FieldConstants {
+    public static double FieldLength = 16.54; //Meters
+    public static double FieldWidth = 8.21; //Meters
+    public static double RobotStartingZone = 1.93; //Distance in meters from the alliance wall to the robot starting zone line
+}
