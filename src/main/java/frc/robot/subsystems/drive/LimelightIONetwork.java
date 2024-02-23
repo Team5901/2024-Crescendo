a7b3f6d8d1547c9dbdec3eb74974b1d275238af5
bauclair22 <59893555+bauclair22@users.noreply.github.com>
2024-02-23 14:42:20 -0500

    SmartDashboard and field constants updates

--- src/main/java/frc/robot/subsystems/drive/LimelightIONetwork.java
+++ src/main/java/frc/robot/subsystems/drive/LimelightIONetwork.java
@@ -4,6 +4,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class LimelightIONetwork implements LimelightIO {
   NetworkTable table;
@@ -19,7 +20,13 @@ public class LimelightIONetwork implements LimelightIO {
       inputs.botPoseWPI = table.getEntry("botpose_wpiblue").getDoubleArray(new double[7]);
     }
     inputs.latency = inputs.botPoseWPI[6];
+    inputs.tv = table.getEntry("tv").getDouble(0);
     // Botpos transform in field-space (driver station WPILIB origin). Translation (X,Y,Z)
     // Rotation(Roll,Pitch,Yaw), total latency (cl+tl)
+  }
+
+  public boolean tagInView(LimelightIOInputs inputs){
+    SmartDashboard.putBoolean("April Tag in View: ", inputs.tv > 0.0);
+    return inputs.tv > 0.0;
   }
 }
