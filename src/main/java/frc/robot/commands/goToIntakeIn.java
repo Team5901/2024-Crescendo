7d2b3c65464ecc9ac683469d2c63f28e359fba75
parchedpacha <kylemagness123@gmail.com>
2024-03-11 21:32:56 -0400

    added intake in and out commands

--- src/main/java/frc/robot/commands/goToIntakeIn.java
+++ src/main/java/frc/robot/commands/goToIntakeIn.java
@@ -1,6 +1,4 @@
 package frc.robot.commands;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.arm.Arm;
@@ -9,81 +7,31 @@ import frc.robot.subsystems.slider.Slider;
 
 public class goToIntakeIn extends SequentialCommandGroup {
   // create method that gracefully extends intake head at low angles to avoid crashing
-
+  private double startAngle;
   public goToIntakeIn(Slider slider, Arm arm) { // ASSUMES we start at intake Out
-    addCommands(
-        new ArmRotateGoToPosition(
-                Constants.ArmSubsystem.armPosIn, Constants.ArmSubsystem.goalTolerance, arm)
-            .withTimeout(1), // Intake rotates arm in.
+    startAngle = arm.getAngle();
+    if (startAngle <5){
+      addCommands(
+          new ArmRotateGoToPosition(
+                  Constants.ArmSubsystem.armPosIn, Constants.ArmSubsystem.goalTolerance, arm)
+              .withTimeout(1), // Intake rotates arm in.
+          new ArmSliderGoToPosition(
+              Constants.SliderSubsystem.sliderIntakeIn,
+              Constants.SliderSubsystem.goalTolerance,
+              slider) // intake Extend's arm in
+          );
+    } else {
+      addCommands(
         new ArmSliderGoToPosition(
             Constants.SliderSubsystem.sliderIntakeIn,
             Constants.SliderSubsystem.goalTolerance,
-            slider) // intake Extend's arm in
-        );
-  }
-
-  // public void goToAimSpeaker() {}
-
-  // public void goToAimAmp() {
-  //   // check if below/at intake out, move to speaker,
-  //   if (startAngle < MovementPositions.IntakeOutDeg) {
-  //     new InstantCommand(() -> arm.setAngleSetPoint(MovementPositions.AimSpeakerDeg))
-  //         .andThen(
-  //             new InstantCommand(() -> arm.setAngleSetPoint(MovementPositions.IntakeInDeg))
-  //                 .andThen(
-  //                     new InstantCommand(
-  //                         () -> slider.setPositionSetPoint(MovementPositions.IntakeInDeg))));
-  //   }
-
-  //   // check if below/at aim speaker, angle
-  //   if (startAngle < MovementPositions.AimSpeakerDeg) {
-  //     new InstantCommand(() -> arm.setAngleSetPoint(MovementPositions.IntakeInDeg))
-  //         .andThen(
-  //             new InstantCommand(
-  //                 () ->
-  //                     slider.setPositionSetPoint(
-  //                         MovementPositions.IntakeInDeg))); // move arm up, THEN in
-  //   }
-
-  //   // check if below/at intake in
-  //   if (startAngle < MovementPositions.IntakeInDeg) {
-  //     new InstantCommand(() -> arm.setAngleSetPoint(MovementPositions.AimAmpDeg));
-  //   }
-  //   // if at aim amp, do nothing/ reset goalpoint
-  //   if (startAngle < MovementPositions.AimAmpDeg) {
-  //     new InstantCommand(() -> arm.setAngleSetPoint(MovementPositions.AimAmpDeg));
-  //   }
-  // }
-
-  public void goToANGLESmartDashboard(Arm arm) {
-    double DashboardVal =
-        SmartDashboard.getNumber("Arm Angle INPUT", Constants.ArmSubsystem.armPosSpeaker);
-    if (DashboardVal < Constants.ArmSubsystem.armSoftLimitLowerAngle) {
-      new ArmRotateGoToPosition(
-          Constants.ArmSubsystem.armSoftLimitLowerAngle, Constants.ArmSubsystem.goalTolerance, arm);
-    } else if (DashboardVal > Constants.ArmSubsystem.armSoftLimitUpperAngle) {
-      new ArmRotateGoToPosition(
-          Constants.ArmSubsystem.armSoftLimitUpperAngle, Constants.ArmSubsystem.goalTolerance, arm);
-    } else {
-      new ArmRotateGoToPosition(DashboardVal, Constants.ArmSubsystem.goalTolerance, arm);
+            slider).alongWith(
+        new ArmRotateGoToPosition(
+            Constants.ArmSubsystem.armPosIn, 
+            Constants.ArmSubsystem.goalTolerance, 
+            arm))); // intake Extend's arm in
     }
   }
 
-  public void goToSLIDERSmartDashboard(Slider slider) {
-    double DashboardVal =
-        SmartDashboard.getNumber("Slider INPUT", Constants.SliderSubsystem.sliderIntakeOut);
-    if (DashboardVal < Constants.SliderSubsystem.sliderSoftLimitLowerInch) {
-      new ArmSliderGoToPosition(
-          Constants.SliderSubsystem.sliderSoftLimitLowerInch,
-          Constants.SliderSubsystem.goalTolerance,
-          slider);
-    } else if (DashboardVal > Constants.SliderSubsystem.sliderSoftLimitUpperInch) {
-      new ArmSliderGoToPosition(
-          Constants.SliderSubsystem.sliderSoftLimitUpperInch,
-          Constants.SliderSubsystem.goalTolerance,
-          slider);
-    } else {
-      new ArmSliderGoToPosition(DashboardVal, Constants.SliderSubsystem.goalTolerance, slider);
-    }
-  }
+  
 }
