e2a7468066e66d505e4d4a3c21a1dc604c54bcd7
Team5901 <35078583+Team5901@users.noreply.github.com>
2024-03-14 21:03:21 -0400

    fixed zeroGyro

--- src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
@@ -43,7 +43,7 @@ public class GyroIOPigeon2 implements GyroIO {
   }
 
   public void zeroGyro() {
-    pigeon.reset();
+    pigeon.setYaw(0);
   }
   // public void calibrateGyro() {
   //   pigeon.
