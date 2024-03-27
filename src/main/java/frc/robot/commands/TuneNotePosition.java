c968050bfdff3098cc5c9ec81b27d107a135d099
parchedpacha <kylemagness123@gmail.com>
2024-03-26 22:23:35 -0400

    new command to pull in notes

--- src/main/java/frc/robot/commands/TuneNotePosition.java
+++ src/main/java/frc/robot/commands/TuneNotePosition.java
@@ -1,25 +1,62 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 import frc.robot.subsystems.intake.Intake;
 
 public class TuneNotePosition extends Command {
     private Intake intake;
-
+    private DigitalInput FrontSensor,RearSensor;
+    private boolean front,rear,changeFlag;
+    private double inSpeed=100;
+    private double outSpeed=-100;
+    private Timer timecheck;
+    private double goalTime=0.25; // seconds to wait 
     public TuneNotePosition(Intake intake, DigitalInput FrontSensor, DigitalInput RearSensor) {
         this.intake=intake;
+        this.FrontSensor=FrontSensor;
+        this.RearSensor=RearSensor;
         addRequirements(intake);
     }
     
     @Override
     public void initialize() {
-
+        timecheck = new Timer();
+        changeFlag=false;
+        
     }
 
     @Override
     public void execute() {
+        front = FrontSensor.get();
+        rear= RearSensor.get();
+        if (front == rear) {
+            timecheck.stop();
+            timecheck.reset();
+        }
+        if (!front && !rear) {
+            intake.runVelocity(inSpeed);
+            changeFlag=true;
+        } else if (front && rear) {
+            intake.runVelocity(outSpeed);
+            changeFlag=true;
+        } else {
+            timecheck.start();
+            intake.stop();
+            if (changeFlag) { // this flag only turns true when we exit our goal area, then is true for one loop while we are in our goal;
+            inSpeed=inSpeed*0.75;// ONE time within our goal area, we slow down the motors a bit.
+            outSpeed=outSpeed*0.75;
+            changeFlag=false;
+            }
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timecheck.get() > goalTime; //if were inside our goal tolerance for a certain amount of time, then call it good and quit
+        
         
     }
 }
