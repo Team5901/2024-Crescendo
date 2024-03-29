e180e971220d5ccc0fe4c8e97d4392048e08c40d
Team5901 <35078583+Team5901@users.noreply.github.com>
2024-03-29 14:45:41 -0400

    added climb pos

---
+++ src/main/java/frc/robot/commands/goToClimbPos.java
@@ -0,0 +1,15 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.slider.Slider;
+
+public class goToClimbPos extends SequentialCommandGroup {
+    public goToClimbPos(Arm arm, Slider slider) {
+        addCommands(
+            new ArmRotateGoToPosition(100, 1, arm).alongWith(
+            new ArmSliderGoToPosition(6.0, 1, slider)).withTimeout(1)
+        );
+    }
+    
+}
